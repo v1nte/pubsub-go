65388c2fa26d1e1845147bf8bfe468ad654ba4a3
v1nte <ivinte99@gmail.com>
2025-07-04 15:47:57 -0400

    feat: initialize DB conn from main.go

--- main.go
+++ main.go
@@ -5,10 +5,17 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/v1nte/pubsub-go/database"
 	"github.com/v1nte/pubsub-go/server"
 )
 
 func main() {
+	if err := database.Init(); err != nil {
+		log.Fatal("Could not connect to DB")
+	}
+
+	defer database.Close()
+
 	broker := server.NewBroker()
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		server.HandleWS(broker, w, r)
