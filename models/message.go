9237bf0046f4ba771191fb0d358cf55e20d59b98
v1nte <ivinte99@gmail.com>
2025-07-04 10:10:55 -0400

    feat: add Message model

---
+++ models/message.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Message struct {
+	ID       primitive.ObjectID `json:"id" bson:"_id"`
+	PostedAt time.Time          `json:"postedAt" bson:"posted_at"`
+	Author   string             `json:"author" bson:"author"`
+	Topic    string             `json:"topic" bson:"topic"`
+	Message  string             `json:"message" bson:"topic"`
+}
